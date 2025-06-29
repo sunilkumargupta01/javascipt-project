39cccca8011dd6a983a09201cce1dd8c150dd989
Sunil Kumar <sunil302628@gmail.com>
2025-06-29 20:52:00 +0530

    commit

---
+++ 2-BMICalculator/script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if(height=== '' || height<0 || isNaN(height)){
+        results.innerHTML = `please give a valid height ${height}`;
+    }
+    if(height=== '' || height<0 || isNaN(height)){
+        results.innerHTML = `please give a valid height ${height}`;
+    }
+})
